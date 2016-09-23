e55786d81eac294ac4c97a9349fd7b097fe4f185
Anderson Araujo <ads.csharp@gmail.com>
2016-09-23 15:23:55 -0300

    voltando alteração. :snake:

--- {{ cookiecutter.nome_do_projeto }}/{{ cookiecutter.nome_do_projeto  }}/settings.py
+++ {{ cookiecutter.nome_do_projeto }}/{{ cookiecutter.nome_do_projeto  }}/settings.py
@@ -8,7 +8,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import json
