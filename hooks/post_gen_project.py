bb249e242fa6630636d91537f59d4afa85c4a560
Anderson Araujo <ads.csharp@gmail.com>
2016-09-23 17:32:49 -0300

    novos testes

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+import binascii
+import os
+
+
+def make_secret_key():
+    print("Criando a secret_key")
+    file = None
+
+    with open("../{{ cookiecutter.nome_do_projeto }}/settings.py", "r") as f:
+        file = f.read()
+
+    secret_key = binascii.hexlify(os.urandom(24))
+
+    file = file.replace("SECRET", secret_key, 1)
+
+    with open("../{{ cookiecutter.nome_do_projeto }}/settings.py", "w") as f:
+        f.write(file)
+    print("Secret Key ok")
+
+
+make_secret_key()
