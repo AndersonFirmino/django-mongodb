46322f0c0b3be90af638dce5890fa9a1d059e8c1
Anderson Araujo <ads.csharp@gmail.com>
2016-09-23 17:35:51 -0300

    novos testes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,18 +4,17 @@ import os
 
 
 def make_secret_key():
-    print("Criando a secret_key")
-    print(os.getcwd())
+    print("Criando a secret_key")    
     file = None
 
-    with open("../{{ cookiecutter.nome_do_projeto }}/settings.py", "r") as f:
+    with open("{{ cookiecutter.nome_do_projeto }}/{{ cookiecutter.nome_do_projeto }}/settings.py", "r") as f:
         file = f.read()
 
     secret_key = binascii.hexlify(os.urandom(24))
 
     file = file.replace("SECRET", secret_key, 1)
 
-    with open("../{{ cookiecutter.nome_do_projeto }}/settings.py", "w") as f:
+    with open("{{ cookiecutter.nome_do_projeto }}/{{ cookiecutter.nome_do_projeto }}/settings.py", "w") as f:
         f.write(file)
     print("Secret Key ok")
 
